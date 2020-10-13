2b15354e40edde4439057501f37af4cbd1ec31b6
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-13 11:11:00 -0500

    refactored issue list to use hooks

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,61 +1,48 @@
-import React, { Component } from "react";
-import Issue from "./Issue";
+import React, { useEffect, useState } from "react";
 import { Route, Link } from "react-router-dom";
+import Issue from "./Issue";
 
-class IssueList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { issues: [] };
-  }
+const IssueList = props => {
+    const [issues, setIssues] = useState([]);
 
-  async componentDidMount() {
-    try {
-      const response = await fetch(
-        "https://api.github.com/repos/facebook/create-react-app/issues"
-      );
-      const data = await response.json();
-      console.log("Issue Array is: ", data);
-      this.setState({
-        issues: data,
-      });
-    } catch (error) {
-      this.setState({
-        issues: error.message,
-      });
-    }
-  }
-
-  render() {
-    const { issues } = this.state;
-
+    useEffect(() => {
+        (async function() {
+            const response = await fetch(
+                "https://api.github.com/repos/facebook/create-react-app/issues"
+              );
+              const issues = await response.json();
+              console.log("Issue Array is: ", issues);
+              setIssues(issues);
+        })();
+    }, [setIssues])
+    
     return (
-      <>
-        {!!issues.length ? (
+        <>
+      {!!issues.length ? (
           <>
-            <h1>Github Issue List</h1>
-            <Route exact path="/">
-                <ul>
-              {issues.map((issue) => {
+          <h1>Github Issue List</h1>
+          <Route exact path="/">
+              <ul>
+            {issues.map((issue) => {
                 return (
-                  <li key={issue.id}>
-                    {issue.title}
-                    <Link to={`/issue/${issue.number}`}>View Details</Link>
-                  </li>
-                );
-              })}
-              </ul>
-            </Route>
-            <Route path={`/issue/:issue_number`}>
-                <Link to='/'>Return to List!</Link>
-                <Issue issues={issues} />
-            </Route>
-          </>
-        ) : (
+                    <li key={issue.id}>
+                  {issue.title}
+                  <Link to={`/issue/${issue.number}`}>View Details</Link>
+                </li>
+              );
+            })}
+            </ul>
+          </Route>
+          <Route path={`/issue/:issue_number`}>
+              <Link to='/'>Return to List!</Link>
+              <Issue issues={issues} />
+          </Route>
+        </>
+      ) : (
           <p>Fetching issues...</p>
-        )}
-      </>
-    );
-  }
+          )}
+    </>
+  );
+  
 }
-
 export default IssueList;
